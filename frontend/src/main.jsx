6a69a7e6620e9adc8e6f9d49fd0f1ad43d9a55ce
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-11-09 18:17:51 +0530

    Import style.css in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import ErrorBoundary from './components/ErrorBoundry.jsx';
 import './index.css';
+import './style.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
